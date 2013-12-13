5411785926c93b40fb49560efab7511b4ddf4b6a
Keith Winstein <keithw@mit.edu>
2013-12-13 13:47:14 -0500

    main.cc: starts up, checks args, and binds socket

--- main.cc
+++ main.cc
@@ -1,4 +1,51 @@
-int main( void )
+/* Goal for this program: send a datagram every given number of seconds,
+                          AND print a message when a datagram is received */
+
+/* by Minna Song and Keith Winstein */
+
+#include <cstdlib>
+#include <iostream>
+
+#include "socket.hh"
+
+using namespace std;
+
+/* arguments: LOCAL_PORT DEST_ADDRESS DEST_PORT INTERVAL */
+int josh( const int argc, const char *argv[] )
 {
-  return 0;
+  /* Truly paranoid check */
+  if ( argc <= 0 ) {
+    throw Exception( "josh", "Missing argv[ 0 ]" );
+  }
+
+  /* Check arguments */
+  if ( argc != 5 ) {
+    throw Exception( argv[ 0 ], "LOCAL_SERVICE DEST_ADDRESS DEST_SERVICE INTERVAL" );
+  }
+
+  /* Get the arguments into our own variables */
+  const string local_service = argv[ 1 ];
+  const string dest_address = argv[ 2 ];
+  const string dest_service = argv[ 3 ];
+  //  const int num_seconds = myatoi( argv[ 4 ] );
+
+  /* Make socket */
+  Socket datagram_socket( UDP );
+
+  /* Bind socket */
+  datagram_socket.bind( Address( "0", local_service, UDP ) );
+
+  /* Start the main loop */
+
+  return EXIT_SUCCESS;
+}
+
+int main( const int argc, const char *argv[] )
+{
+  try {
+    return josh( argc, argv );
+  } catch ( const Exception & e ) {
+    e.perror();
+    return EXIT_FAILURE;
+  }
 }
